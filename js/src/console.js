2b319cdbd01b1220f26d2e126cb7b66606dd9aed
xingji.yjq <xingji.yjq@antfin.com>
2019-02-23 15:10:08 +0800

    Site updated: 2019-02-23 15:10:07

--- js/src/console.js
+++ js/src/console.js
@@ -1,3 +1,3 @@
 console.log("欢迎观光前端-小强博客。\n%c关于我: http://xiaoqiang730730.github.io/about/", "color:red");
 console.log("%cfollow me: https://github.com/xiaoqiang730730", "color:#4078c0");
-console.log("博主建了前端学习微信群。http://xiaoqiang730730.github.io/images/wechat.jpeg \n加好友%cxiaoqiangauto 备注博客", "color:red");
+console.log("博主建了前端学习微信群。http://xiaoqiang730730.github.io/images/wechat.jpeg \n加好友%cy838871837 备注博客", "color:red");
